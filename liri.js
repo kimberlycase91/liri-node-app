f68a774590c016303b10574f2eb4cba295a0e874
kimberlycase91 <kimberlycase91@gmail.com>
2020-02-12 09:17:17 -0700

    finished concert-this function to output the desired data only, added .catch for errors

--- liri.js
+++ liri.js
@@ -19,9 +19,33 @@ function concert() {
     var artist = inputArray.join(" ").replace(",","")
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     console.log(artist)
-    axios.get(queryURL).then(function(response){
-        console.log(response)
-    })
+    axios.get(queryURL).then(function(response){ 
+        for (i=0; i < 10; i++) {
+        console.log(response.data[i].venue.name);
+        console.log(response.data[i].venue.city);
+        console.log(response.data[i].datetime);
+    }
+})
+.catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Sorry, this band doesn't have any upcoming shows.");
+    }
+    console.log(error.config);
+  });
 }
 
 switch (process.argv[2]) {

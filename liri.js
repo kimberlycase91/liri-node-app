deafb395a01a6509c7ff546561a034e65243c6c7
kimberlycase91 <kimberlycase91@gmail.com>
2020-02-12 08:56:59 -0700

    set up concert-this function and switch case-currently makes axios call, then returns the response

--- liri.js
+++ liri.js
@@ -1,27 +1,34 @@
 require("dotenv").config();
 
-var keys = require("keys.js");
+var keys = require("./keys");
 
-var spotify = new Spotify(keys.spotify);
+var spotify = keys.spotify;
 
 var axios = require("axios");
 
 var input = process.argv;
 var inputArray = [];
 
-for (i=2; i < input.length; i++) {
-    
+for (i=3; i < input.length; i++) {
+  inputArray.push(input[i])
 }
+console.log(inputArray);
 
 //node liri.js concert-this <artist/band name here>
-function concert(input) {
-    var artist = input;
+function concert() {
+    var artist = inputArray.join(" ").replace(",","")
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
+    console.log(artist)
     axios.get(queryURL).then(function(response){
         console.log(response)
     })
 }
+
+switch (process.argv[2]) {
+    case "concert-this": 
+    concert();
+}
+
 
 //node liri.js spotify-this-song '<song name here>'
 function spotify() {
